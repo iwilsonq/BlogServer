953579461eebbf0f734fb2dde440742d47ffd677
Ian Wilson <iwilsonq@gmail.com>
2017-02-06 22:45:30 -0800

    public assets

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
@@ -10,7 +11,10 @@ const mongoose = require('mongoose');
 // DB setup
 mongoose.connect('mongodb://localhost/journies');
 
+app.use(express.static('public'));
+
 // App setup
+app.use(cors());
 app.use(morgan('combined'));
 app.use(bodyParser.json({type: '*/*'}));
 router(app);
--- test/test_helper.js
+++ test/test_helper.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 before(done => {
-  mongoose.connect('mongodb://localhost/journies');
+  mongoose.connect('mongodb://localhost/journies_test');
   mongoose.connection
     .once('open', () => done())
     .on('error', error => console.warn('Warning', error))
